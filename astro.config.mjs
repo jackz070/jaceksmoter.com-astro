2c6d13ae25dc9563ad970dadeb23d70926510635
jackz070 <95173354+jackz070@users.noreply.github.com>
2023-01-26 01:27:09 +0100

    Add github pages support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [prefetch()]
+  integrations: [prefetch()],
+  site: "https://jackz070.github.io",
+  base: "/jaceksmoter.com-astro",
 });
